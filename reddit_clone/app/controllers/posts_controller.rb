79548e5f209a11a0e5602f7310129cf67b7d7ca9
sdossettswift <sdossettswift@gmail.com>
2016-06-26 11:15:49 -0500

    added posts controller with actions for index, create, update, view, delete, comment. vote_up, vote_down

--- reddit_clone/app/controllers/posts_controller.rb
+++ reddit_clone/app/controllers/posts_controller.rb
@@ -1,25 +1,25 @@
 class PostsController < ApplicationController
-  def index
-  end
+    def index
+    end
 
-  def create
-  end
+    def create
+    end
 
-  def update
-  end
+    def update
+    end
 
-  def view
-  end
+    def view
+    end
 
-  def delete
-  end
+    def delete
+    end
 
-  def comment
-  end
+    def comment
+    end
 
-  def vote_up
-  end
+    def vote_up
+    end
 
-  def vote_down
-  end
+    def vote_down
+    end
 end
