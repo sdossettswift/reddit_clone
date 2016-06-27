5f170ac4ed9c6313754741ddc15098887545059a
sdossettswift <sdossettswift@gmail.com>
2016-06-27 01:47:24 -0500

    votes work

--- reddit_clone/config/routes.rb
+++ reddit_clone/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
     devise_for :users
-    resources :links
-        root to: "links#index"
+    resources :links do
+        member do
+            put "like", to:"links#upvote"
+            put "dislike", to:"links#downvote"
+        end
+    end
+
+    root to: "links#index"
 end
